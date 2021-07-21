33e068828a2b0cd4bd4fd14aecec85e63155f7c6
Arpit Dixit <arpitd@dewsolutions.in>
2021-07-21 15:25:12 +0530

    Initial Commit

---
+++ SwiftUI-Flags_of_the_world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Flags_of_the_world
+//
+//  Created by Arpit Dixit on 21/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Flags_of_the_world/SwiftUI_Flags_of_the_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Flags_of_the_worldApp.swift
+//  SwiftUI-Flags_of_the_world
+//
+//  Created by Arpit Dixit on 21/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Flags_of_the_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
