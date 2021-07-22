2cb79ba4682ad7926156df04837bc23c647054c2
Arpit Dixit <arpitd@dewsolutions.in>
2021-07-22 12:08:28 +0530

    Added Flag to modal

--- SwiftUI-Flags_of_the_world/ContentView.swift
+++ SwiftUI-Flags_of_the_world/ContentView.swift
@@ -10,13 +10,42 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var showModal = false
+    @State private var selectedFlag = ""
+    let flags = [["🇦🇫", "Country 1"], ["🇦🇽", "Country 2"], ["🇦🇱", "Country 3"], ["🇩🇿", "Country 4"], ["🇦🇸", "Country 5"], ["🇦🇩", "Country 6"]]
     
     var body: some View {
-        Button("Show Modal") {
-            showModal.toggle()
-        }.sheet(isPresented: $showModal, content: {
-            Text("Modal Opened")
-        })
+        VStack {
+            List {
+                ForEach(flags, id: \.self) { flag in
+                    HStack {
+                        Text(flag[0])
+                            .font(.custom("Arial", size: 100))
+                        Text(flag[1])
+                            .font(.title)
+                            .padding()
+                    }.onTapGesture {
+                        showModal.toggle()
+                        selectedFlag = flag[0]
+                    }
+                }
+            }
+            .sheet(isPresented: $showModal, content: {
+                Text(selectedFlag)
+                    .font(.custom("Arial", size: 200))
+            })
+            
+            HStack {
+                Button("Show Modal") {
+                    showModal.toggle()
+                }.sheet(isPresented: $showModal, content: {
+                    Text("Modal Opened")
+                })
+                .font(.title)
+                .padding(10)
+                .background(Color.blue)
+                .foregroundColor(.white)
+            }
+        }
     }
 }
 
